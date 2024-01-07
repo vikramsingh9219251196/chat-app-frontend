f951762b7bc089795eafee8098f59ec197e2af48
vikramsingh9219251196 <bobvik2003@gmail.com>
2024-01-08 02:22:00 +0530

    first commit

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "http://localhost:5000";
+export const loginRoute = `${host}/api/v1/auth/login`;
+export const registerRoute = `${host}/api/v1/auth/register`;
+export const logoutRoute = `${host}/api/v1/auth/logout`;
+export const allUsersRoute = `${host}/api/v1/auth/allusers`;
+export const sendMessageRoute = `${host}/api/v1/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/v1/messages/getmsg`;
+export const setAvatarRoute = `${host}/api/v1/auth/setavatar`;
